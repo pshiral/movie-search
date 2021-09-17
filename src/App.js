9f8bd52207c942d7db9ecded9600996c52807919
Parshuram Shiral <parshurams@hcl.com>
2021-09-17 21:59:53 +0530

    updating readme and coding changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [options, setOptions] = useState([]),
+    [selected, setSelected] = useState(null);
+
+  const debounce = (cb) => {
+    let id = null;
+
+    return (...args) => {
+      clearTimeout(id);
+
+      id = setTimeout(() => {
+        cb.apply(null, args);
+      }, 300);
+    };
+  };
+
+  const handleChange = useCallback(debounce((event) => {
+    let text = event?.target?.value;
+
+    if (text.length > 2) {
+      fetch(`http://www.omdbapi.com/?s=${text}&apiKey=74de3445`).then((response) => {
+        return response.json();
+      }).then((response) => {
+        const result = response?.Search?.slice(0, 5);
+
+        if (result) {
+          setOptions(response.Search.slice(0, 5));
+        } else {
+          setOptions([]);
+        }
+      });
+    } else {
+      setOptions([]);
+    }
+  }), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h2 className="heading">
+          Web Developer Assignment
+        </h2>
+        <span className="by">Sr Software Engineer</span>
       </header>
+      <main>
+        <form autoComplete="off">
+          <section className="autocomplete">
+            <input type="text" name="places" placeholder="Search a place"
+              data-test-id="input" onChange={handleChange} />
+            <div className="options">
+              {options?.map((movie) => (
+                <div onClick={() => {
+                  setOptions([]);
+                  setSelected(movie);
+                }}>{movie.Title}</div>
+              ))}
+            </div>
+          </section>
+          {selected && <section className="card">
+            <img alt={selected.Title} src={selected.Poster} />
+            <div className="title">
+              <span>Title: {selected.Title}</span>
+              <span>Year: {selected.Year}</span>
+            </div>
+          </section>}
+        </form>
+      </main>
     </div>
   );
 }
