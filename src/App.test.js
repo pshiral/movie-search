0e049e9eb4a1c3599b5c1dac4d3340f32fa11ba0
Parshuram Shiral <parshurams@hcl.com>
2021-09-17 23:12:08 +0530

    unit tests cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Card from './Card';
 
 describe('App', () => {
   let wrapper;
 
   beforeEach(() => wrapper = shallow(<App />));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+
+  it('should have a root element', () => {
+    expect(wrapper.find('div.app').length).toEqual(1);
   });
 
-  it('should render the Calculator Component', () => {
-    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
+  it('should render card correctly', () => {
+    const selected = { Title: 'My test', Year: '2021', Poster: 'test' };
+
+    const component = shallow(<Card selected={selected} />);
+    expect(component).toMatchSnapshot();
   });
 });
